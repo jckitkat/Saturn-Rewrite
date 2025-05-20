cfceebc7767f75e9cf82c715d976733b8d08766f
jckitkat <jcmertz@outlook.com>
2025-05-20 11:38:51 -0400

    got rid of commented code in robotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 // import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 // import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -53,127 +54,51 @@ import frc.robot.utils.Constants;
 import frc.robot.utils.Constants.IO;
 
 public class RobotContainer {
-  
-  public enum FaceLocation {
-    None(0),
-    Speaker(1), 
-    Pass(2);
 
-    int faceLocation;
-    private FaceLocation(int faceLocation) {
-      this.faceLocation = faceLocation;
+    public enum FaceLocation {
+        None(0),
+        Speaker(1),
+        Pass(2);
+
+        int faceLocation;
+
+        private FaceLocation(int faceLocation) {
+            this.faceLocation = faceLocation;
+        }
     }
-  }
-  
-  // public final NoteLight lights = new NoteLight();
-  // public final Vision vision = new Vision();
-  //public final PhotonNoteDetection noteDetection = new PhotonNoteDetection(vision.getRingCam());
-  public final Drivetrain drivetrain = Drivetrain.getInstance();
-  // private final Intake intake = Intake.getInstance();
-  // private final Elevator elevator = Elevator.getInstance();
-  // public final Shooter shooter = Shooter.getInstance();
-  // private final Climber climber = Climber.getInstance();
 
-  PS5Controller driver = new PS5Controller(0);
-  PS5Controller operator = new PS5Controller(1);
+    public final Drivetrain drivetrain = Drivetrain.getInstance();
 
-  GenericHID guitar = new GenericHID(2);
+    PS5Controller driver = new PS5Controller(0);
+    PS5Controller operator = new PS5Controller(1);
 
-  // SendableChooser<Command> autoChooser;
+    GenericHID guitar = new GenericHID(2);
 
-  FaceLocation faceLocation = FaceLocation.None;
+    FaceLocation faceLocation = FaceLocation.None;
 
-  public RobotContainer() {
-    configureBindings();
-    drivetrain.setDefaultCommand(new TeleopDrive(
-      drivetrain,
-      () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveXAxis), Constants.IO.swerveDeadband),
-      () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveYAxis), Constants.IO.swerveDeadband),
-      () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveOmegaAxis), Constants.IO.swerveDeadband),
-      () -> true,
-      () -> (faceLocation == FaceLocation.Speaker) ? (DriverStation.getAlliance().get() == Alliance.Blue ? Constants.FieldLocations.blueSpeaker : Constants.FieldLocations.redSpeaker) : Constants.FieldLocations.none,
-      () -> MathUtil.applyDeadband(driver.getRawAxis(5), 0.5),
-      () -> MathUtil.applyDeadband(driver.getRawAxis(2), 0.5)
-    ));
-    // intake.setDefaultCommand(new RunCommand(() -> intake.setLights(operator.getRawAxis(1)), intake));
-    // shooter.setDefaultCommand(new RunCommand(() -> shooter.setPositionSpeed(operator.getRawAxis(1)), shooter));
-    // elevator.setDefaultCommand(new RunCommand(() -> elevator.setSpeed(MathUtil.applyDeadband(-operator.getRawAxis(5), Constants.IO.elevatorDeadband)), elevator));
-    // climber.setDefaultCommand(new RunCommand(() -> climber.setArmSpeed(MathUtil.applyDeadband(-operator.getRawAxis(1), Constants.IO.climberDeadband)), climber));
+    public RobotContainer() {
+        configureBindings();
+        drivetrain.setDefaultCommand(new TeleopDrive(
+                drivetrain,
+                () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveXAxis), Constants.IO.swerveDeadband),
+                () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveYAxis), Constants.IO.swerveDeadband),
+                () -> MathUtil.applyDeadband(-driver.getRawAxis(IO.driveOmegaAxis), Constants.IO.swerveDeadband),
+                () -> true,
+                () -> (faceLocation == FaceLocation.Speaker)
+                        ? (DriverStation.getAlliance().get() == Alliance.Blue ? Constants.FieldLocations.blueSpeaker
+                                : Constants.FieldLocations.redSpeaker)
+                        : Constants.FieldLocations.none,
+                () -> MathUtil.applyDeadband(driver.getRawAxis(5), 0.5),
+                () -> MathUtil.applyDeadband(driver.getRawAxis(2), 0.5)));
+    }
 
-    // NamedCommands.registerCommand("Start", new InstantCommand(() -> {shooter.moveTo(Shooter.PositionState.SHOOT); shooter. setShootSpeed(80); intake.moveTo(Intake.PositionState.GROUND); intake.setRollerSpeed(0.3);}).andThen(new WaitCommand(0.5)).andThen(new InstantCommand(() -> shooter.setFeederSpeed(0.3))).andThen(new InstantCommand(() -> {shooter.moveTo(Shooter.PositionState.SCORE_3);})));
-    // NamedCommands.registerCommand("Side Score", new InstantCommand(() -> {shooter.moveTo(Shooter.PositionState.SIDE_SCORE); shooter. setShootSpeed(80); intake.moveTo(Intake.PositionState.STOW); intake.setRollerSpeed(0.3);}).andThen(new WaitCommand(0.3)).andThen(new InstantCommand(() -> shooter.setFeederSpeed(0.3))).andThen(new InstantCommand(() -> {shooter.moveTo(Shooter.PositionState.HANDOFF);})));
-    // NamedCommands.registerCommand("Wait For Ring", new InstantCommand(() -> {intake.moveTo(Intake.PositionState.GROUND); intake.setRollerSpeed(0.3);}).andThen(new WaitUntilCommand(() -> intake.hasGamePiece()).andThen(new InstantCommand(() -> {intake.setRollerSpeed(0);})).andThen(new SetIntakePosition(Intake.PositionState.STOW)).andThen(new InstantCommand(() -> {intake.setRollerSpeed(0.4); shooter.setFeederSpeed(0.3);}))).andThen(new WaitCommand(1)));
-    // NamedCommands.registerCommand("Wait For Far Ring", new InstantCommand(() -> {intake.moveTo(Intake.PositionState.GROUND); intake.setRollerSpeed(0.3);}).andThen(new WaitUntilCommand(() -> intake.hasGamePiece()).andThen(new InstantCommand(() -> {intake.setRollerSpeed(0);})).andThen(new SetIntakePosition(Intake.PositionState.STOW))));
-    // NamedCommands.registerCommand("Intake", new InstantCommand(() -> {intake.moveTo(Intake.PositionState.GROUND); intake.setRollerSpeed(0.3);}));
-    // NamedCommands.registerCommand("End", new InstantCommand(() -> {shooter.setShootSpeed(0); shooter.setFeederSpeed(0); shooter.moveTo(Shooter.PositionState.HANDOFF); intake.setRollerSpeed(0); intake.moveTo(Intake.PositionState.STOW);}));
-    // NamedCommands.registerCommand("Intake", new GetRingAuton());
-    // NamedCommands.registerCommand("Prepare Close Shot", new PrepareCloseShotAuton());
-    // NamedCommands.registerCommand("Prepare Side Shot", new PrepareSideShotAuton());
-    // NamedCommands.registerCommand("Prepare Far Shot", new PrepareFarShotAuton());
-    // NamedCommands.registerCommand("Shoot", new ShootAuton());
-    // NamedCommands.registerCommand("Intake And Shoot", new IntakeAndShoot());
-    // NamedCommands.registerCommand("Turn Off Apriltags", new InstantCommand(() -> vision.turnOffAprilTags()));
-    // NamedCommands.registerCommand("Turn On Apriltags", new InstantCommand(() -> vision.turnOnAprilTags()));
-    
-    // autoChooser = AutoBuilder.buildAutoChooser();
-    // SmartDashboard.putData("Auto Chooser", autoChooser);
+    private void configureBindings() {
+        Trigger resetGyroButton = new Trigger(() -> driver.getRawButton(IO.resetGyroButton));
 
-    // PortForwarder.add(1182, null, 0);
+        resetGyroButton.onTrue(new InstantCommand(drivetrain::zeroGyro));
+    }
 
-  }
-
-
-  private void configureBindings() {
-    // Command intakeCommand = new AutoIntake(operator);
-    Trigger resetGyroButton = new Trigger(() -> driver.getRawButton(IO.resetGyroButton));
-    // Trigger shootAutoAimButton = new Trigger(() -> driver.getRawButton(5));
-
-    // Trigger intakeButton = new Trigger(() -> operator.getRawButton(IO.intakeButton));
-    // // Trigger intakeButton = new Trigger(() -> driver.getRawButton(IO.intakeButton));
-    // Trigger cancelIntakeButton = new Trigger(() -> operator.getRawButton(IO.cancelIntakeButton));
-    // // Trigger cancelIntakeButton = new Trigger(() -> driver.getRawButton(IO.cancelIntakeButton));
-    // Trigger reverseIntakeButton = new Trigger(() -> operator.getRawButton(2));
-    // Trigger manualIntake = new Trigger(() -> {return operator.getPOV() == 0;});
-    // Trigger prepareSpeakerButton = new Trigger(() -> operator.getRawButton(IO.prepareSpeakerButton));
-    // Trigger passButton = new Trigger(() -> operator.getRawButton(1));
-    // Trigger shortPassButton = new Trigger(() -> operator.getRawButton(10));
-    // Trigger prepareAmpButton = new Trigger(() -> operator.getRawButton(IO.prepareAmpButton));
-    // // Trigger shootButton = new Trigger(() -> driver.getRawButton(IO.shootButton));
-    // Trigger shootButton = new Trigger(() -> operator.getRawButton(IO.shootButton));
-    
-    resetGyroButton.onTrue(new InstantCommand(drivetrain::zeroGyro));
-    // // shootAutoAimButton.onTrue(new ShootAutoAim(() -> shootButton.getAsBoolean()).alongWith(new InstantCommand(() -> faceLocation = FaceLocation.Speaker)).finallyDo(() -> faceLocation = FaceLocation.None));
-    // shootAutoAimButton.onFalse(new InstantCommand(() -> faceLocation = FaceLocation.None));
-
-    // intakeButton.onTrue(new InstantCommand(() -> intakeCommand.cancel()).andThen(intakeCommand));
-    // cancelIntakeButton.onTrue(new SetIntakePosition(Intake.PositionState.STOW).alongWith(new SetShooterPosition(Shooter.PositionState.HANDOFF)).alongWith(new InstantCommand(() -> {intakeCommand.cancel(); intake.setRollerSpeed(0); shooter.setFeederSpeed(0); shooter.setShootSpeed(0); shooter.reset();})));
-    // reverseIntakeButton.onTrue(new InstantCommand(() -> intake.setRollerSpeed(-0.3)));
-    // reverseIntakeButton.onFalse(new InstantCommand(() -> intake.setRollerSpeed(0)));
-    // manualIntake.onTrue(new InstantCommand(() -> {intake.setRollerSpeed(0.2); shooter.setFeederSpeed(0.2);}));
-    // manualIntake.onFalse(new InstantCommand(() -> {intake.setRollerSpeed(0); shooter.setFeederSpeed(0);}));
-    // // prepareSpeakerButton.onTrue(new ShootAutoAim(() -> shootButton.getAsBoolean()));
-    // //prepareSpeakerButton.onTrue(new SetShooterPosition(Shooter.PositionState.AUTO).alongWith(new SetIntakePosition(Intake.PositionState.SCORING)).alongWith(new SequentialCommandGroup(new InstantCommand(() -> {shooter.setFeederSpeed(-0.2); shooter.setShootSpeed(-2);}), new WaitCommand(0.1))).andThen(new InstantCommand(() -> {shooter.setShootSpeed(0); shooter.setFeederSpeed(0);})).andThen(new RunCommand(() -> shooter.setPositionSpeed(operator.getRawAxis(1)))).alongWith(new RunCommand(() -> shooter.setShootSpeed(80))));
-    // prepareSpeakerButton.onTrue(new Shoot(() -> shootButton.getAsBoolean()));
-    // //shootButton.onTrue(new RunCommand(() -> shooter.setFeederSpeed(0.3)));
-    // passButton.onTrue(new Pass(() -> shootButton.getAsBoolean()));
-    // shortPassButton.onTrue(new ShortPass(() -> shootButton.getAsBoolean()));
-    // prepareAmpButton.onTrue(new Amp(() -> shootButton.getAsBoolean()));
-    // shootButton.onTrue(new InstantCommand(() -> intake.flashLights(0, 0)));
-    // new Trigger(() -> operator.getRawButton(9)).onTrue(new InstantCommand(() -> shooter.reset()));
-    // new Trigger(() -> operator.getRawButton(14)).onTrue(new SetIntakePosition(Intake.PositionState.GROUND).alongWith(new SetShooterPosition(Shooter.PositionState.CLIMB)));
-    // new Trigger(() -> operator.getRawButton(10)).onTrue(new SetIntakePosition(Intake.PositionState.STOW).alongWith(new SetShooterPosition(Shooter.PositionState.AMP)));
-    // new Trigger(() -> operator.getRawButton(3)).onTrue(new InstantCommand(() -> {intake.setRollerSpeed(0.2); shooter.setFeederSpeed(0.2); shooter.setShootSpeed(-2);}).andThen(new WaitCommand(0.4)).andThen(new WaitUntilCommand(() -> intake.getTorqueCurrent() < 25)).andThen(new InstantCommand(() -> {intake.setRollerSpeed(0.0); shooter.setFeederSpeed(0.0); shooter.setShootSpeed(0); SmartDashboard.putBoolean("Intake Ring", false); SmartDashboard.putBoolean("Ready to Shoot", true);})));
-    // // guitar controls
-    // new Trigger(() -> guitar.getRawButton(3)).whileTrue(new InstantCommand(() -> lights.setColor("Red")));
-    // new Trigger(() -> guitar.getRawButton(9)).onTrue(new InstantCommand(() -> lights.setColor("Orange")));
-    // new Trigger(() -> guitar.getRawButton(4)).onTrue(new InstantCommand(() -> lights.setColor("Yellow")));
-    // new Trigger(() -> guitar.getRawButton(2)).onTrue(new InstantCommand(() -> lights.setColor("Green")));
-    // new Trigger(() -> guitar.getRawButton(1)).onTrue(new InstantCommand(() -> lights.setColor("Blue")));
-    // new Trigger(() -> guitar.getRawButton(5)).onTrue(new InstantCommand(() -> lights.setColor("Violet")));
-  }
-
-
-  // public Command getAutonomousCommand() {
-  //   // return autoChooser.getSelected();
-  // }
+    public Command getAutonomousCommand() {
+        return Commands.none();
+    }
 }
